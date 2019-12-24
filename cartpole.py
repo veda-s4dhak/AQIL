5389aa99bb13b3918aaf07e76134376f2e42c8bc
Xiao Lei Zhang <zhxl0903@gmail.com>
2019-12-24 14:02:33 -0500

    Added weight plot for IL250

--- cartpole.py
+++ cartpole.py
@@ -427,8 +427,8 @@ if __name__ == "__main__":
     # cartpole.run()
 
     config = dict()
-    config['model_name'] = "RL250"
-    config['n_episodes'] = 250
+    config['model_name'] = "IL250"
+    config['n_episodes'] = -1
     config['user_imitation_mode'] = False
     config['pid_imitation_mode'] = False
 
