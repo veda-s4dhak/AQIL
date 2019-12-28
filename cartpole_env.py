0e82b3899cf4256352332a106aaaf4eb16bc3ae8
Xiao Lei Zhang <zhxl0903@gmail.com>
2019-12-28 16:25:44 -0500

    Added explanation for reward

--- cartpole_env.py
+++ cartpole_env.py
@@ -132,6 +132,8 @@ class CartPoleEnv(gym.Env):
                 # Even if angle is high in magnitude, reward may not be low if the machine action is equal to the user action
                 # This is is so that the DQN is optimizes for future reward
                 # The Bellman will optimize for best long term user action prediction
+
+                # Learning a reward ditribution based on model action's similarity to action of PID controller.
                 reward = 0.2*self.gaussian_function(x=self.theta, sigma=np.deg2rad(10), mu=0) + 0.8*self.gaussian_function(x=action, sigma=np.deg2rad(10), mu=user_input)
                 # reward = self.gaussian_function(x=action, sigma=np.deg2rad(10), mu=user_input)
 
